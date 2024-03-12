754fe6bb4625abd1d9f0e452b511c05a252f0cd6
Nanskip <nanskip.oof@gmail.com>
2024-03-12 13:36:48 +0200

    add ambience

---
+++ scripts/ambience.lua
@@ -0,0 +1,24 @@
+require("ambience"):set({
+	sky = {
+		skyColor = Color(0,0,0),
+		horizonColor = Color(0,0,0),
+		abyssColor = Color(0,0,0),
+		lightColor = Color(152,152,161),
+		lightIntensity = 1.000000,
+	},
+	fog = {
+		color = Color(0,0,0),
+		near = 1100,
+		far = 0,
+		lightAbsorbtion = 0.400000,
+	},
+	sun = {
+		color = Color(255,255,255),
+		intensity = 0.010000,
+		rotation = Number3(1.061162, 3.089219, 0.000000),
+	},
+	ambient = {
+		skyLightFactor = 0.350000,
+		dirLightFactor = 0.200000,
+	}
+})
--- scripts/start.lua
+++ scripts/start.lua
@@ -2,3 +2,4 @@
 -- This is start function, better set everything that must be started once only here.
 
 Camera:SetParent(World)
+setAmbience()
