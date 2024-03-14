e5dcebce7b999357ecd3407dc6599c3aab707d16
Nanskip <nanskip.oof@gmail.com>
2024-03-14 13:05:03 +0200

    set camera rotation and position

--- scripts/start.lua
+++ scripts/start.lua
@@ -1,5 +1,8 @@
 
--- This is start function, better set everything that must be started once only here.
+-- This is start function, better set everything that must be started only once here.
 
 Camera:SetParent(World)
+Camera:SetModeFree()
+Camera.Position = Number3(25, 55, 0)
+Camera.Rotation = Number3(0, 0, 0)
 setAmbience()
