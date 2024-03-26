3c44e1d8cdfbd1abf7c04c4db05bae0b7848bc0b
Nanskip <nanskip.oof@gmail.com>
2024-03-26 20:51:02 +0200

    init restart() at loading

--- scripts/start.lua
+++ scripts/start.lua
@@ -16,6 +16,7 @@ enemiesTable = {}
 ui = require("uikit")
 hud.init()
 hud.update()
+restart()
 
 Screen.DidResize = function()
     Camera.FOV = math.max(math.min(40*((Screen.Width/Screen.Height+0.45)/0.9), 100), 60)*0.4
