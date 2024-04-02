a40873dcdb781cd6557b8709151150583b4f910d
Nanskipp <104688738+Nanskipp@users.noreply.github.com>
2024-04-02 19:08:41 +0300

    Update enemies.lua

--- scripts/enemies.lua
+++ scripts/enemies.lua
@@ -118,7 +118,7 @@ enemies.spawn = function(count)
         enemies.create({
             position = Number3(math.random(5, 45), 110 + math.random(0, 20), 45),
             rotation = Rotation(0, 0, 0),
-            constantMovementAcceleration = Number3(math.random(-15, 15)*-0.05, math.random(5, 15)*-0.003, 0),
+            constantMovementAcceleration = Number3(math.random(-15, 15)*-0.005, math.random(5, 15)*-0.003, 0),
             constantRotationAcceleration = Rotation(math.random(0, 20)*0.001, math.random(0, 20)*0.001, math.random(0, 20)*0.001),
             color = Color(math.random(0, 100)*0.01, math.random(0, 100)*0.01, math.random(0, 100)*0.01, 1.0),
             scale = Number3(1*randScaleMultiplier, 1*randScaleMultiplier, 1*randScaleMultiplier),
